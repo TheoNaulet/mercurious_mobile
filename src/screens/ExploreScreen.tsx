dd1501af25d9c478bb633b1a0a435cd958deb7e5
Naulet Théo <theo.naulet@gmail.com>
2023-07-18 15:06:01 +0200

    feat(explorescreen): add data loading to scroll

--- src/screens/ExploreScreen.tsx
+++ src/screens/ExploreScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { StyleSheet, ScrollView } from 'react-native';
 
 import { getCountries, getCountriesByContinent } from "../api/country";
@@ -7,19 +7,46 @@ import ExploreCard from '../components/cards/ExploreCard';
 import ButtonList from '../components/utils/ButtonList';
 
 const ExploreScreen = () => {
-
+  const scrollRef = useRef();
   const continents = ['Afrique', 'Amérique', 'Asie', 'Europe', 'Océanie'];
   
 	const [continent, setContinent] = useState("");
   const [country, setCountry] = useState("");
 	const [countryList, setCountryList] = useState([]);
 	const [cityList, setCityList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [cityPage, setCityPage] = useState(1);
+
+
+  const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
+    const paddingToBottom = 20;
+    return layoutMeasurement.height + contentOffset.y >=
+      contentSize.height - paddingToBottom;
+  };
+  
+  const handleFetchMoreCountries = async () => {
+    if (!continent) {
+      const newPage = page + 1;
+      setPage(newPage);
+      const moreCountries = await getCountries(newPage);
+      setCountryList(oldCountries => [...oldCountries, ...moreCountries]);
+    }
+  };
+  
+  const handleFetchMoreCities = async () => {
+    if(!country){
+      const newPage = cityPage + 1;
+      setCityPage(newPage);
+      const moreCities = await getAllCities(newPage);
+      setCityList(oldCities => [...oldCities, ...moreCities]);
+    }
+  };
 
 
 	const handleFetchPlaces = async () => {
-    const responseCountries = await getCountries(); 
-    const responseCities = await getAllCities();
-    
+    const responseCountries = await getCountries(1); 
+    const responseCities = await getAllCities(1);
+
     setCityList(responseCities); 
 		setCountryList(responseCountries);
 	};
@@ -55,16 +82,33 @@ const ExploreScreen = () => {
   return (
     <ScrollView contentContainerStyle={styles.contentContainer}>
       <ButtonList item={continent} setItem={setContinent} list={continents}/>
-      <ScrollView showsHorizontalScrollIndicator={false} alwaysBounceVertical={false} contentContainerStyle={styles.countriesContainer}>
+      <ScrollView 
+        ref={scrollRef}
+        onScroll={({nativeEvent}) => {
+          if (isCloseToBottom(nativeEvent)) {
+            handleFetchMoreCountries();
+          }
+        }}
+        scrollEventThrottle={400}
+        showsHorizontalScrollIndicator={false} alwaysBounceVertical={false} contentContainerStyle={styles.countriesContainer}>
         {countryList.map((val, index) => (
             <ExploreCard Name={val.countryName} CardImage={val.image} key={index} type="country"/>
           ))}
       </ScrollView>
       <ButtonList item={country} setItem={setCountry} list={countryList.map(country => country.countryName)}/>
-      <ScrollView showsHorizontalScrollIndicator={false} alwaysBounceVertical={false} contentContainerStyle={styles.citiesContainer}>
+      <ScrollView 
+        showsHorizontalScrollIndicator={false} 
+        alwaysBounceVertical={false} 
+        contentContainerStyle={styles.citiesContainer}
+        onScroll={({nativeEvent}) => {
+            if (isCloseToBottom(nativeEvent)) {
+                handleFetchMoreCities();
+            }
+        }}
+        scrollEventThrottle={400}>
         {cityList.map((val, index) => (
             <ExploreCard Name={val?.Name} CardImage={val?.Image} key={index} type="city"/>
-          ))}
+        ))}
       </ScrollView>
     </ScrollView>
   );
