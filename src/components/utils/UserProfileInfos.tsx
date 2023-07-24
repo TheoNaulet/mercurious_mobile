cf4a3b1b41c34b4f595943e3e7ff28890fe33b43
Naulet Théo <theo.naulet@gmail.com>
2023-07-24 13:50:19 +0200

    feat(profile): add possibility to change username

--- src/components/utils/UserProfileInfos.tsx
+++ src/components/utils/UserProfileInfos.tsx
@@ -1,20 +1,56 @@
 import React, { useContext, useState } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, Modal, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Modal, TouchableWithoutFeedback, Platform, Alert } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { handleImageSelection } from '../../utils/imagePickerHelper';
 import { uploadImage } from '../../api/upload';
 import { FIREBASE_AUTH } from '../../../FirebaseConfig';
-import { UserContext } from '../../context/UserContext';
+import { updateUsername } from '../../api/user';
+import { UserContext } from '../../../context/UserContext';
+import StatsBlock from './StatsBlock';
 
 const EditProfileModal: React.FC<EditProfileModalProps> = ({ visible, onClose }) => {
     const auth = FIREBASE_AUTH; 
     const uid = auth?.currentUser?.uid; 
+    const { setUpdate } = useContext(UserContext);
 
     const onImageSelected = async (uri) => {
         try {
             await uploadImage(uid, { uri: uri });
+            setUpdate(prevUpdate => prevUpdate + 1);
         } catch (error) {
             console.error("Failed to upload image", error);
+        }
+    };
+
+    const onUsernameEntered = async () => {
+        if (Platform.OS === 'ios') {
+            Alert.prompt(
+                'Changer d\'Username',
+                'Entrez votre nouvel username :',
+                [
+                    {
+                        text: 'Annuler',
+                        style: 'cancel',
+                    },
+                    {
+                        text: 'Confirmer',
+                        onPress: async (newUsername) => {
+                            try {
+                                const response = await updateUsername(uid, newUsername);
+                                setUpdate(prevUpdate => prevUpdate + 1);
+                                if (response.message){
+                                    console.log(response.message);
+                                }
+                            } catch (error) {
+                                console.error('Erreur lors de la mise à jour du nom d\'utilisateur', error);
+                            }
+                        }
+                    },
+                ],
+                'plain-text',
+            );
+        } else {
+            console.log('La fonction onUsernameEntered doit être implémentée pour Android.');
         }
     };
 
@@ -34,7 +70,7 @@ const EditProfileModal: React.FC<EditProfileModalProps> = ({ visible, onClose })
                         </View>
                         <Text style={styles.modalText}>Changer de photo de profil</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.modalOption} onPress={() => {}}>
+                    <TouchableOpacity style={styles.modalOption} onPress={() => onUsernameEntered()}>
                         <View style={styles.iconContainer}>
                             <Icon type='font-awesome' name="user" size={20} color="black" />
                         </View>
@@ -63,8 +99,9 @@ interface UserProfileInfoProps {
  * @param {UserProfileInfoProps} props The properties for the UserProfileInfo component.
  * @returns {JSX.Element} Returns a JSX element representing user's profile information.
  */
-const UserProfileInfo= ({ username, picture, numberCountries, followings, followers }: UserProfileInfoProps): JSX.Element => {
+const UserProfileInfo= ({ id, username, picture, numberCountries, followings, followers, navigation }: UserProfileInfoProps): JSX.Element => {
     const [modalVisible, setModalVisible] = useState(false);
+    const { user } = useContext(UserContext);
 
     return (
         <View style={styles.profileInfos}>
@@ -76,29 +113,21 @@ const UserProfileInfo= ({ username, picture, numberCountries, followings, follow
                     />
                 </View>
                 <View style={styles.statsContainer}>
-                    <View style={styles.statsBlock}>
-                        <Text style={styles.statsNumber}>{numberCountries}</Text>
-                        <Text style={styles.statsLabel}>Pays</Text>
-                    </View>
-            
-                    <View style={styles.statsBlock}>
-                        <Text style={styles.statsNumber}>{followers?.length}</Text>
-                        <Text style={styles.statsLabel}>Followers</Text>
-                    </View>
-            
-                    <View style={styles.statsBlock}>
-                        <Text style={styles.statsNumber}>{followings?.length}</Text>
-                        <Text style={styles.statsLabel}>Suivi</Text>
-                    </View>
+                    <StatsBlock countries={numberCountries} followers={followers} followings={followings} navigation={ navigation } />
                 </View>
             </View>
             <View style={styles.bottomContainer}>
                 <Text style={styles.accountName}>{username && `@${username}`}</Text>
-                <TouchableOpacity style={styles.editButton} onPress={() => setModalVisible(true)}>
-                    <Icon type='font-awesome' name="pencil" size={20} color="white" />
-                    <Text style={styles.editButtonText}>Modifier le profil</Text>
-                </TouchableOpacity>
-                <EditProfileModal visible={modalVisible} onClose={() => setModalVisible(false)} />
+                {user === id && (
+                    
+                    <>
+                        <TouchableOpacity style={styles.editButton} onPress={() => setModalVisible(true)}>
+                            <Icon type='font-awesome' name="pencil" size={20} color="white" />
+                            <Text style={styles.editButtonText}>Modifier le profil</Text>
+                        </TouchableOpacity>
+                        <EditProfileModal visible={modalVisible} onClose={() => setModalVisible(false)} />
+                    </>
+                )}
             </View>
         </View>
     );
