b78ebf48d5d4abc1b09c29a350cce107f054f6d8
Naulet Théo <theo.naulet@gmail.com>
2023-07-27 09:15:07 +0200

    fix(visit): remove modal from isfriendvisit

--- src/components/utils/isFriendVisit.tsx
+++ src/components/utils/isFriendVisit.tsx
@@ -4,6 +4,7 @@ import { getUsersById } from '../../api/user';
 import { Image } from 'react-native';
 import { StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
+import Visitors from './Visitors';
 
 /**
  * Component to check if friends have visited a specific place.
@@ -11,9 +12,8 @@ import { Icon } from 'react-native-elements';
  * @param {{visitorsId: Array<string>, navigation: Object}} props - The properties passed to this component.
  * @returns {JSX.Element} - A JSX element that displays the number of friends who have visited a place.
  */
-const FriendVisit = ({ visitorsId, navigation }: { visitorsId: array, navigation: Object }): JSX.Element => {
+const FriendVisit = ({ visitorsId, navigation }: { visitorsId: Array<string>, navigation: Object }): JSX.Element => {
     const [visitors, setVisitors] = useState([]); 
-    const [modalVisible, setModalVisible] = useState(false);
 
     const fetchVisitors = async () => {
         if(visitorsId?.length > 0) {
@@ -22,20 +22,6 @@ const FriendVisit = ({ visitorsId, navigation }: { visitorsId: array, navigation
             return;
         }
         return; 
-    }
-
-    const closeModal = () => {
-        setModalVisible(false);
-    }
-
-    const goToUserPage = (userId) => {
-        closeModal();
-        navigation.navigate('FriendScreen', { User: userId });
-    }
-
-
-    const handleOpenModal = () => {
-        setModalVisible(true);
     }
 
     useEffect(()=>{
@@ -54,54 +40,9 @@ const FriendVisit = ({ visitorsId, navigation }: { visitorsId: array, navigation
                 ? "1 ami a visité ce lieu"
                 : `${visitorsId?.length} amis ont visité ce lieu`}
             </Text>
-            <View style={styles.imagesContainer}>
-                {visitors.slice(0, 3).map((visitor, index) => (
-                    <TouchableOpacity 
-                        key={index} 
-                        style={styles.imageTouch} 
-                        onPress={handleOpenModal}
-                    >
-                        <Image 
-                        style={styles.image}
-                            source={{ uri: visitor.Profile_picture ? visitor.Profile_picture : defaultPicture }} 
-                        />
-                    </TouchableOpacity>
-                ))}
-                {visitors.length > 3 && (
-                    <View style={styles.moreVisitorsView}>
-                        <Text style={styles.moreVisitorsText}>+{visitors.length - 3}</Text>
-                    </View>
-                )}
-            </View>
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={closeModal}
-            >
-                <TouchableOpacity style={styles.centeredView} activeOpacity={1}>
-                    <View style={styles.modalView}>
-                        <View style={styles.modalTitle}>
-                            <TouchableOpacity onPress={closeModal} style={styles.iconContainer}>
-                                <Icon type='font-awesome' name="arrow-left" />
-                            </TouchableOpacity>
-                            <Text style={styles.titleText}>Amis ayant visité ce lieu</Text>
-                        </View>
-                        <FlatList
-                            data={visitors}
-                            keyExtractor={item => item.userId}
-                            renderItem={({ item }) => (
-                                <TouchableOpacity onPress={() => goToUserPage(item.userId)}>
-                                    <View style={styles.userRow}>
-                                        <Image style={styles.userImage} source={{ uri: item.Profile_picture ? item.Profile_picture : defaultPicture }} />
-                                        <Text style={styles.userName}>{item.Username}</Text>
-                                    </View>
-                                </TouchableOpacity>
-                            )}
-                        />
-                    </View>
-                </TouchableOpacity>
-            </Modal>
+            {
+                visitorsId?.length > 0 ? <Visitors visitors={visitors} navigation={navigation} type='lieu' /> : <></>
+            }
         </View>
     );
 };
@@ -113,30 +54,6 @@ const styles = StyleSheet.create({
     },
     text: {
         marginRight: 10,
-    },
-    imagesContainer: {
-        flexDirection: 'row',
-        marginLeft:10
-    },
-    imageTouch: {
-        marginLeft: -10,
-    },
-    image: {
-            width: 30,
-            height: 30,
-            borderRadius: 15,
-    },
-    moreVisitorsView: {
-        backgroundColor: 'gray',
-        borderRadius: 15,
-        width: 30,
-        height: 30,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: -10
-    },
-    moreVisitorsText: {
-        color: 'white'
     },
     centeredView: {
         flex: 1,
